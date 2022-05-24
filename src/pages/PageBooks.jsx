c2c19a6a5fe0eb3254ec2f393e0e0465faa99351
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-24 11:55:21 +0200

    finished

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FaSpinner } from 'react-icons/fa';
+import FadeIn from 'react-fade-in';
 
 const url = 'https://ejt-livecoding-apisite-back.herokuapp.com';
 
@@ -7,34 +9,48 @@ export const PageBooks = () => {
 	const [books, setBooks] = useState([]);
 
 	useEffect(() => {
-		(async () => {
-			setBooks((await axios.get(url)).data);
-		})();
+		setTimeout(() => {
+			(async () => {
+				setBooks((await axios.get(url)).data);
+			})();
+		}, 0);
 	}, []);
 
 	return (
 		<>
 			<h2>Books</h2>
-			<p>There are {books.length} books:</p>
+			<FadeIn>
+				{books.length === 0 ? (
+					<div>
+						<FaSpinner className="spinner" />
+					</div>
+				) : (
+					<>
+						<p>There are {books.length} books:</p>
 
-			<div className="books">
-				{books.map((book, index) => {
-					return (
-						<div key={index} className="book">
-							<img
-								src={`https://edwardtanguay.netlify.app/share/images/techBooks/${book.idCode}.jpg`}
-								alt="book"
-							/>
-							<div className="info">
-								<div className="title">{book.title}</div>
-								<div className="description">
-									{book.description}
-								</div>
-							</div>
+						<div className="books">
+							{books.map((book, index) => {
+								return (
+									<div key={index} className="book">
+										<img
+											src={`https://edwardtanguay.netlify.app/share/images/techBooks/${book.idCode}.jpg`}
+											alt="book"
+										/>
+										<div className="info">
+											<div className="title">
+												{book.title}
+											</div>
+											<div className="description">
+												{book.description}
+											</div>
+										</div>
+									</div>
+								);
+							})}
 						</div>
-					);
-				})}
-			</div>
+					</>
+				)}
+			</FadeIn>
 		</>
 	);
 };
