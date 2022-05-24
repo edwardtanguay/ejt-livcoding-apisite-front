9038c41a55dd74ebf967820a1bf51d1ef187c58f
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-24 11:45:38 +0200

    styled with images

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -6,24 +6,33 @@ const url = 'https://ejt-livecoding-apisite-back.herokuapp.com';
 export const PageBooks = () => {
 	const [books, setBooks] = useState([]);
 
-useEffect(() => {
-	(async () => {
-		setBooks((await axios.get(url)).data);
-	})();
-}, []);
+	useEffect(() => {
+		(async () => {
+			setBooks((await axios.get(url)).data);
+		})();
+	}, []);
 
 	return (
 		<>
 			<h2>Books</h2>
 			<p>There are {books.length} books:</p>
-		
+
 			<div className="books">
 				{books.map((book, index) => {
 					return (
-						<div className="book">
-							<div className="title">{book.title}</div>
+						<div key={index} className="book">
+							<img
+								src={`https://edwardtanguay.netlify.app/share/images/techBooks/${book.idCode}.jpg`}
+								alt="book"
+							/>
+							<div className="info">
+								<div className="title">{book.title}</div>
+								<div className="description">
+									{book.description}
+								</div>
+							</div>
 						</div>
-					)
+					);
 				})}
 			</div>
 		</>
