3ddc6449839217af587c999ed2b22c211ca566d3
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-24 11:39:01 +0200

    add api link

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const url = 'https://ejt-livecoding-apisite-back.herokuapp.com';
+
 export const PageBooks = () => {
+	const [books, setBooks] = useState([]);
+
+useEffect(() => {
+	(async () => {
+		setBooks((await axios.get(url)).data);
+	})();
+}, []);
+
 	return (
 		<>
 			<h2>Books</h2>
-			<p>This is the books page.</p>
-			<img src="images/bashCookbook.jpg"/>
+			<p>There are {books.length} books:</p>
+		
+			<div className="books">
+				{books.map((book, index) => {
+					return (
+						<div className="book">
+							<div className="title">{book.title}</div>
+						</div>
+					)
+				})}
+			</div>
 		</>
 	);
 };
